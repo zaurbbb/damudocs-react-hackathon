c6a660f8e36690640b551951a2d68947890f0f08
zaurbbb <zaurberdibekov@mail.ru>
2022-12-04 13:02:11 +0600

    fix conflicts x2

--- src/components/pages/doctor/Content/login/LoginUpper.jsx
+++ src/components/pages/doctor/Content/login/LoginUpper.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import Graph from './images/Graph.svg';
 
 const LoginUpper = () => {
     return (
         <>
-            <p className='p-h1'>Добрый день!</p>
-            <p className='p-h5-right'>Всего принято пациентов - 42</p>
-            <img src={Graph} alt=""/>
+            <p className='p-h1'>Войти в систему!</p>
         </>
     );
 };
